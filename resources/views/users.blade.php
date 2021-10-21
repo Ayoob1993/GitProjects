770a48f9905e1b455b9d7bf1f7e13ac36efb9e37
unknown <mohamadayoob365@gmail.com>
2021-10-21 17:28:16 -0400

    First Laravel Project

---
+++ resources/views/users.blade.php
@@ -0,0 +1,9 @@
+<h1>User Login</h1>
+
+{{$errors}}
+<form action="users" method="POST">
+    @csrf
+    <input type="text" name="username" placeholder="enter user"><br><br>
+    <input type="password" name="userpassword" placeholder="enter user password"><br><br>
+    <button type="submit">Login</button>
+</form>
